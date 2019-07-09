d51807db81ccf231d3c37819f7d07ed288aeafef
DONGWON KIM <lunacircle4@gmail.com>
2019-07-10 00:43:56 +0900

    practice

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function(){
+        return (
+            <div className="header">
+            <div className="header-content">
+                <p>hello</p>
+            </div>
+            <style jsx>{`
+                p {
+                    color: red;
+                }
+            `}</style>
+            </div >
+        )
+}
